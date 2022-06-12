4ba346dd80d8927353c20b4dc1f0da0a7306d51c
parul1311 <parulsagrawal@gmail.com>
2022-06-12 17:42:09 +0530

    initial commit

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -5,6 +5,8 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+    
   
     <div class="collapse navbar-collapse justify-content-end collapse " id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
