204db3cd61937e762936717b43c295dc5497a5e8
Parul Agrawal <parulsagrawal@gmail.com>
2023-03-18 19:58:41 +0530

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,9 @@
    <div class="hero">
         <div class="hero-text">
             <h2> Assignment Test</h2>
+            <h2> Pratyush Nalayak</h2>
+            <h2> Pratyush Dhor</h2>
+            <h2> Pratyush Dusht</h2>
         </div>
    </div>
    
